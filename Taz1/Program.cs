d9317902121f8cf12a1fed6ffb9685c88a22fe1b
Angelikam <an_lev@yahoo.com>
2023-01-27 15:11:16 +0300

    DZ

---
+++ Taz1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Введите число :");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int numRevers = num % 10 * 10000 + num % 100 / 10 * 1000 + num % 1000 / 100 * 100 + num % 10000 / 1000 * 10 + num % 100000 / 10000;
+
+if  (num == numRevers)
+{
+    System.Console.WriteLine($"{num} -> Да");
+}
+else
+{
+    System.Console.WriteLine($"{num} -> Нет");
+}
+
